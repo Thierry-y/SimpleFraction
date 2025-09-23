c4711935ace89cb7bfee8bffcc90c85f9b0a3c59
Tianyi Yang <thierry.yang2002@gmail.com>
2025-09-23 15:05:00 +0100

    fonc toString and test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Fraction f = new Fraction();
+        
+        assert f.toString().equals("Je suis une fraction.") 
+            : "Échec : toString() ne retourne pas la chaîne attendue";
+        
+        System.out.println(f);
     }
 }
